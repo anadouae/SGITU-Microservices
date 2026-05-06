5549135c76ca4c5a3dbc1d7a5e7205a8151697d2
siham <siham.emf35@gmail.com>
2026-05-06 05:14:15 +0100

    ajout d'un import at d'une annotation dans G5Application.java

--- service-notification/src/main/java/ma/sgitu/g5/G5Application.java
+++ service-notification/src/main/java/ma/sgitu/g5/G5Application.java
@@ -2,7 +2,9 @@ package ma.sgitu.g5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class G5Application {
 
